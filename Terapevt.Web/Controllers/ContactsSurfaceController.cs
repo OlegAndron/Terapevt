12f35f4cd5fe68f2719b097afa534c11c3145442
OlegAndron <hdd@tut.by>
2014-06-30 04:14:51 +0300

    c
    Signed-off-by: OlegAndron <hdd@tut.by>

--- Terapevt.Web/Controllers/ContactsSurfaceController.cs
+++ Terapevt.Web/Controllers/ContactsSurfaceController.cs
@@ -9,12 +9,13 @@ namespace Terapevt.Web.Controllers
     public class ContactsSurfaceController : SurfaceController
     {
         [HttpPost]
-        public ActionResult SendMessage(EmailMessageModel message)
+        public ActionResult SendMessage(EmailMessageModel model)
         {
             var contactsNode = new Node(1140);
             contactsNode.GetProperty("Email");
+            var x = model.From;
             var client = new SmtpClient("mail.google.com");
-            return RedirectToCurrentUmbracoPage();
+            return Redirect("/");
         }
     }
 }
